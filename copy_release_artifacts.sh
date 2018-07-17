98ca9c1fb5f94c2b127e0c9427a8fc572c478dd7
gevashkar <gevashkar.rampersadh@electrum.co.za>
2018-07-17 11:14:35 +0200

    Circle 2 update

--- copy_release_artifacts.sh
+++ copy_release_artifacts.sh
@@ -1,44 +1,42 @@
-# collects release artifacts and copies them to the build Directory
+#!/bin/bash
+
+# collects release artifacts and copies them to the release directory
 
 projectFolder="."
-buildName=$(mvn help:evaluate -Dexpression=project.name | tail -8 | head -1)
-buildVersion=$(mvn help:evaluate -Dexpression=project.version | tail -8 | head -1)
+buildName=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.artifactId}' --non-recursive exec:exec)
+buildVersion=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
 
 # check all is in order
-if [ ! -e ${projectFolder}/target/${buildName}-${buildVersion}-javadoc.jar ]
-then
-  echo "File ${projectFolder}/target/${buildName}-${buildVersion}-javadoc.jar not found: exiting"
-  exit 1
-fi
-if [ ! -e ${projectFolder}/target/${buildName}-${buildVersion}-sources.jar ]
-then
-  echo "File ${projectFolder}/target/${buildName}-${buildVersion}-sources.jar not found: exiting"
-  exit 1
-fi
-if [ ! -e ${projectFolder}/target/${buildName}-${buildVersion}.jar ]
-then
-  echo "File ${projectFolder}/target/${buildName}-${buildVersion}.jar not found: exiting"
-  exit 1
-fi
 if [ ! -e ${projectFolder}/target/${buildName}-${buildVersion}.tar.gz ]
 then
   echo "File ${projectFolder}/target/${buildName}-${buildVersion}.tar.gz not found: exiting"
   exit 1
 fi
 
-# make the builds directory pristine again
-if [ ! -d ${projectFolder}/builds ]
+RPM_TARGET_PATH=${projectFolder}/target/rpm/electrum-${buildName}/RPMS/x86_64
+RPM_NAME=electrum-${buildName}-${buildVersion}-1.x86_64.rpm
+if [ ! -e ${RPM_TARGET_PATH}/${RPM_NAME} ]
 then
-  echo "Creating builds folder"
-  mkdir ${projectFolder}/builds
+  RPM_NAME=electrum-${buildName}-${buildVersion}_1.x86_64.rpm
+fi
+if [ ! -e ${RPM_TARGET_PATH}/${RPM_NAME} ]
+then
+  echo "File $RPM_NAME not found: exiting"
+  exit 1
+fi
+
+# make the release directory pristine again
+if [ ! -d ${projectFolder}/release ]
+then
+  echo "Creating release folder"
+  mkdir ${projectFolder}/release
 fi
 
 echo "Removing old files"
-rm -rf ${projectFolder}/builds/*
+rm -rf ${projectFolder}/release/*
 
-# copy artifacts
-echo "Copying artifacts"
-cp ${projectFolder}/target/${buildName}-${buildVersion}-javadoc.jar builds
-cp ${projectFolder}/target/${buildName}-${buildVersion}-sources.jar builds
-cp ${projectFolder}/target/${buildName}-${buildVersion}.jar builds
-cp ${projectFolder}/target/${buildName}-${buildVersion}.tar.gz builds
+echo "Copying release artifacts"
+cp ${projectFolder}/target/${buildName}-${buildVersion}.tar.gz release
+md5sum release/${buildName}-${buildVersion}.tar.gz | awk '{ print $1 }' > release/${buildName}-${buildVersion}.md5
+cp ${RPM_TARGET_PATH}/${RPM_NAME} release
+md5sum release/${RPM_NAME} | awk '{ print $1 }' > release/${RPM_NAME}.md5
