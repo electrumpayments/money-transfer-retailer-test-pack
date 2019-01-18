fc244f9ae695833beb06b680e1e8cc6d2ab9b87b
neilmackellar <neilmackellar@electrum.co.za>
2019-01-18 09:22:26 +0200

    Align v2.4 (#2)
    * update dependencies
    
    * update Heroku Postman environment

--- heroku_run.sh
+++ heroku_run.sh
@@ -1,1 +1,1 @@
-java $JAVA_OPTS -jar target/money-transfer-retailer-test-pack-1.0.0-SNAPSHOT-jar-with-dependencies.jar $PORT
+java $JAVA_OPTS -jar target/money-transfer-retailer-test-pack-1.1.0-jar-with-dependencies.jar $PORT
